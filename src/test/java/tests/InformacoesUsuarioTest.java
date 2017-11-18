3198fe8e984d95bd466d1d17f82532f3d4d575bd
Angelo Medeiros <angelomedeiros3@gmail.com>
2017-11-18 05:21:03 -0300

    Adicionando contato

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -53,12 +54,39 @@ public class InformacoesUsuarioTest {
 
         // Validar que dentro do elemento com class "me" está como texto "Hi, Julio"
 
-        WebElement me = navegador.findElement(By.className("me"));
-        String textoNoElementoMe = me.getText().toLowerCase();
-        assertEquals("hi, julio",textoNoElementoMe);
+//        WebElement me = navegador.findElement(By.className("me"));
+//        String textoNoElementoMe = me.getText().toLowerCase();
+//        assertEquals("hi, julio",textoNoElementoMe);
 
+        // Clicar no link com texto "Hi, Julio"
 
+        navegador.findElement(By.linkText("Hi, Julio")).click();
 
+        // Clicar no link com texto "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+        // Clicar no button com xpath "//button[@data-target="addmoredata"]"
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
+
+        // Identificar o popup de id "addmoredata"
+        WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
+
+        // No select de name "type" escolher a opção "Phone"
+        WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+
+        // Método para manipular select
+        new Select(campoType).selectByVisibleText("Phone");
+
+        // No campo de name "contact" digitar "+5583986000000
+        popupAddMoreData.findElement(By.name("contact")).sendKeys("+5583986000000");
+
+        // Clicar no link de text "SAVE" que está na popup
+        popupAddMoreData.findElement(By.linkText("SAVE")).click();
+
+        // Validar no id de "toast-container" que o texto é "Your contact has been added!"
+        WebElement mensagemPopup = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPopup.getText();
+        assertEquals("Your contact has been added!",mensagem);
 
         // Validando teste!
         // Assert.assertEquals( 1,2); // Se no import for -> import org.junit.Assert;
@@ -68,7 +96,7 @@ public class InformacoesUsuarioTest {
     @After
     public void tearDown(){
         // Fechar navegador;
-       // navegador.quit();
+        navegador.quit();
     }
 }
 
